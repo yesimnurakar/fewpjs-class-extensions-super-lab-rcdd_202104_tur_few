e359e387968eed007089bda484bdcdc4ed767410
Yesim nur Akar <yesimnurakar@gmail.com>
2021-05-26 08:04:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your classes here
+class Tree {
+  constructor(species) {
+    this.species = species;
+  }
+
+
+  static definition() {
+    return `A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.`;
+  }
+}
+
+// Inherits from Pet
+class  Deciduous extends Tree {
+  constructor(species, name) {
+    super(species);
+    this.name = name;
+
+  }
+
+  static definition() {
+    return (
+      super.definition() + ' Deciduous trees shed their leaves annually.'
+    );
+  }
+}
+class  Evergreen extends Tree {
+  constructor(species, name) {
+    super(species);
+        this.name = name;
+   }
+
+  static definition() {
+    return (
+      super.definition() + ' Evergreens keep their leaves all year round.'
+    );
+  }
+}
+
+
+Evergreen.definition();
+Deciduous.definition();
