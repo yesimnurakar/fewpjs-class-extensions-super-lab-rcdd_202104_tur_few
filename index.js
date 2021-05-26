0fe830658bbda9c38d104dbb8b33d247ff495eec
Yesim nur Akar <yesimnurakar@gmail.com>
2021-05-26 07:51:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your classes here
+class Tree {
+  constructor(species) {
+    this.species = species;
+  }
+ 
+
+  static definition() {
+    return `A tree is a perennial plant with an elongated stem, or trunk, supporting
+ branches and leaves.`;
+  }
+}
+ 
+// Inherits from Pet
+class  Deciduous extends Tree {
+  constructor(species, name) {
+    super(name);
+    super(species)= species;
+  }
+ 
+  static definition() {
+    return (
+      super.definition() + ' Deciduous trees shed their leaves annually.'
+    );
+  }
+}
+ 
+let creature = new Pet('The Thing');
+let dog = new Dog('Spot', 'foxhound');
+ 
+Pet.definition();
+Dog.definition();
